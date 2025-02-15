ff5b1bbba1be3e219c6a0f147ce074a2979577c4
todaytooth1 <todaytooth1@gmail.com>
2025-02-14 18:38:05 -0800

    Inital committ

---
+++ .idea/script.js
@@ -0,0 +1,11 @@
+//Initialize Map
+var map = L.map('map', {
+    crs: L.CRS.Simple,
+    minZoom: -2,
+    maxZoom: 2
+});
+
+//Define Image Size
+
+var imgWidth = 5000, imgHeight = 5000;
+var imageUrl =
